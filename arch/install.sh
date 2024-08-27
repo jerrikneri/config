3cb5e4d4a3649a7763facaf120d0335d7fe2600f
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2024-08-26 17:17:27 -0700

    move macos config

--- arch/install.sh
+++ arch/install.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
+source ./zsh/.zshenv
+
 ########
 # nvim #
 ########
-mkdir -p "$HOME/.config/nvim"
-mkdir -p "$HOME/.config/nvim/undo"
+mkdir -p "$XDG_CONFIG_HOME/nvim"
+mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
-ln -sf "$HOME/dotfiles/nvim/init.nvim" "$HOME/.config/nvim"
+ln -sf "$DOTFILES/nvim/init.nvim" "$XDG_CONFIG_HOME/nvim"
 # -s symbolic otherwise it will create a hard link
 # -f force creation of link, remove existing if any
 
 # Link entire directory -f not needed as we wipe existing, and can't be used on directories
-rm -rf "$HOME/.config/X11"
-ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+rm -rf "$XDG_CONFIG_HOME/X11"
+ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
 
 #######
 # zsh #
 #######
 
-mkdir -p "$HOME/.config/zsh"
-ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
-ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
-ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+mkdir -p "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
+ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 
-rm -rf "$HOME/.config/zsh/external"
-ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
+rm -rf "$XDG_CONFIG_HOME/zsh/external"
+ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
