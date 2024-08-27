a2cde8aabca4994e1cca594cac1e06a10be4e2b8
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2024-08-26 18:52:23 -0700

    use correct alias now that .config is nested

--- install_arch.sh
+++ install_arch.sh
@@ -6,13 +6,13 @@
 mkdir -p "$XDG_CONFIG_HOME/nvim"
 mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
-ln -sf "$DOTFILES/nvim/init.nvim" "$XDG_CONFIG_HOME/nvim"
+ln -sf "$DOTFILES_CONFIG/nvim/init.nvim" "$XDG_CONFIG_HOME/nvim"
 # -s symbolic otherwise it will create a hard link
 # -f force creation of link, remove existing if any
 
 # Link entire directory -f not needed as we wipe existing, and can't be used on directories
 rm -rf "$XDG_CONFIG_HOME/X11"
-ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
+ln -s "$DOTFILES_CONFIG/X11" "$XDG_CONFIG_HOME"
 
 
 #######
@@ -20,9 +20,12 @@ ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 #######
 
 mkdir -p "$XDG_CONFIG_HOME/zsh"
-ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
-ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
-ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
+
+# clean existing
+rm $XDG_CONFIG_HOME/zsh/.zshrc
+
+ln -sf "$DOTFILES_CONFIG/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES_CONFIG/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
-ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES_CONFIG/zsh/external" "$XDG_CONFIG_HOME/zsh"
