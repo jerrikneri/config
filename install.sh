a8915332d82f0cbd8a50e6e1a7cbf48459df7976
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2024-08-26 18:15:23 -0700

    use existing .env or create

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+
+# Source base environment variables for establishing directorie
 source .config/zsh/.zshenv
 
+# Set .zshenv in ~
 cp .config/zsh/.zshenv $HOME
+
+# Check if .env does not exist
+if [ ! -f .env ]; then
+    # Copy .env.example to .env
+    cp .env.example .env
+    echo ".env file created from .env.example"
+else
+    echo ".env file already exists"
+fi
 
 # Environment Variables
 source $DOTFILES/.env
