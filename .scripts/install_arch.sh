d68b0588f78296d6dfbe2d590fa6d22a5b277563
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2024-08-27 17:27:25 -0700

    commit tmux on arch

--- .scripts/install_arch.sh
+++ .scripts/install_arch.sh
@@ -22,6 +22,13 @@ ln -sf "$DOTFILES_CONFIG/nvim/init.nvim" "$XDG_CONFIG_HOME/nvim"
 rm -rf "$XDG_CONFIG_HOME/X11"
 ln -s "$DOTFILES_CONFIG/X11" "$XDG_CONFIG_HOME"
 
+########
+# tmux #
+########
+
+mkdir -p "$XDG_CONFIG_HOME/tmux"
+ln -sf $DOTFILES_CONFIG/tmux $XDG_CONFIG_HOME
+
 #########
 # fonts #
 #########
