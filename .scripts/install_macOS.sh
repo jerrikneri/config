687cf90b5984ab35522d4b1ddf0bb9fb3a3d4cf2
Jerrik <16113652+jerrikneri@users.noreply.github.com>
2024-08-27 17:06:58 -0700

    use XDG_CONFIG_HOME for simplicity and one source of truth

--- .scripts/install_macOS.sh
+++ .scripts/install_macOS.sh
@@ -1,14 +1,15 @@
 ln -sf $DOTFILES_CONFIG/zsh/.zshenv $HOME/.zshenv
 
-ln -sf $DOTFILES_CONFIG/alacritty $HOME/.config
-ln -sf $DOTFILES_CONFIG/karabiner $HOME/.config
-ln -sf $DOTFILES_CONFIG/newsboat $HOME/.config
-ln -sf $DOTFILES_CONFIG/nvim $HOME/.config
-ln -sf $DOTFILES_CONFIG/sketchybar $HOME/.config
-ln -sf $DOTFILES_CONFIG/tmux $HOME/.config
-ln -sf $DOTFILES_CONFIG/skhd $HOME/.config
-ln -sf $DOTFILES_CONFIG/yabai $HOME/.config
-ln -sf $DOTFILES_CONFIG/zsh/.zshrc $HOME/.config/zsh/.zshrc
+ln -sf $DOTFILES_CONFIG/alacritty $XDG_CONFIG_HOME
+ln -sf $DOTFILES_CONFIG/karabiner $XDG_CONFIG_HOME
+ln -sf $DOTFILES_CONFIG/newsboat $XDG_CONFIG_HOME
+ln -sf $DOTFILES_CONFIG/nvim $XDG_CONFIG_HOME
+ln -sf $DOTFILES_CONFIG/sketchybar $XDG_CONFIG_HOME
+ln -sf $DOTFILES_CONFIG/tmux $XDG_CONFIG_HOME
+ln -sf $DOTFILES_CONFIG/tmuxinator $XDG_CONFIG_HOME
+ln -sf $DOTFILES_CONFIG/skhd $XDG_CONFIG_HOME
+ln -sf $DOTFILES_CONFIG/yabai $XDG_CONFIG_HOME
+ln -sf $DOTFILES_CONFIG/zsh/.zshrc $XDG_CONFIG_HOME/zsh/.zshrc
 
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES_CONFIG/zsh/external" "$ZDOTDIR"
